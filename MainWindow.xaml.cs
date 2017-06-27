fd2beb63482a91ad13075662347cd9994b3d386d
YanBingxin <775127481@qq.com>
2017-06-27 18:29:09 +0800

    魔板游戏，采用三轮换 生成新盘保证可恢复性
    魔板游戏，采用三轮换 生成新盘保证可恢复性

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MagicPan
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool changed = false;
+        /// <summary>
+        /// 空盘
+        /// </summary>
+        private PanKey panNull = new PanKey() { Template = null, Value = 16, Content = 16, IsEnabled = false, X = 3, Y = 3 };
+        /// <summary>
+        /// 全键盘
+        /// </summary>
+        private List<PanKey> pans = new List<PanKey>();
+        DispatcherTimer timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            if (!changed)
+                return;
+            gd.Children.Clear();
+            foreach (PanKey p in pans)
+            {
+                p.Content = p.Value.ToString();
+                Grid.SetRow(p, p.Y);
+                Grid.SetColumn(p, p.X);
+                gd.Children.Add(p);
+            }
+            changed = false;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            CreatePan();
+            timer.Tick += timer_Tick;
+        }
+
+        public void CreatePan()
+        {
+            count = 0;
+            pans.Clear();
+            panNull = new PanKey() { Template = null, Value = 16, Content = 16, IsEnabled = false, X = 3, Y = 3 };
+            pans.Add(panNull);
+            //生成对象
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3 || (j < 4 && i < 3); j++)
+                {
+                    PanKey pk = new PanKey();
+                    pk.X = i;
+                    pk.Y = j;
+                    pk.Value = i + 4 * j + 1;
+                    pans.Add(pk);
+                }
+            }
+            //sanlunhuan
+            for (int s = 0; s < 1400; s++)
+            {
+                int i = new Random().Next(0, 14);
+                PanKey tem = new PanKey();
+                tem.X = pans[i].X;
+                tem.Y = pans[i].Y;
+                pans[i].X = pans[i + 1].X;
+                pans[i].Y = pans[i + 1].Y;
+                pans[i + 1].X = pans[i + 2].X;
+                pans[i + 1].Y = pans[i + 2].Y;
+                pans[i + 2].X = tem.X;
+                pans[i + 2].Y = tem.Y;
+                Console.WriteLine(i);
+            }
+            changed = true;
+            timer.Start();
+        }
+        int count = 0;
+        void timer_Tick(object sender, EventArgs e)
+        {
+            count++;
+            time.Content = TimeSpan.FromSeconds(count);
+        }
+
+        private void gd_Click(object sender, RoutedEventArgs e)
+        {
+            PanKey pan = e.Source as PanKey;
+            if (pan.Value == 16)
+            {
+                return;
+            }
+            if (Math.Abs(pan.X - panNull.X) + Math.Abs(pan.Y - panNull.Y) == 1)
+            {
+                int x = pan.X;
+                int y = pan.Y;
+                pan.X = panNull.X;
+                pan.Y = panNull.Y;
+                panNull.X = x;
+                panNull.Y = y;
+                changed = true;
+            }
+
+            foreach (PanKey p in pans)
+            {
+                if (!p.CheckLocation())
+                {
+                    return;
+                }
+            }
+            panNull.Template = pan.Template;
+            MessageBox.Show("你赢了！");
+            timer.Stop();
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CreatePan();
+        }
+
+    }
+}
---
+++ PanKey.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MagicPan
+{
+    public class PanKey : Button
+    {
+
+        public int X
+        {
+            get { return (int)GetValue(XProperty); }
+            set { SetValue(XProperty, value); }
+        }
+
+        public int Y
+        {
+            get { return (int)GetValue(YProperty); }
+            set { SetValue(YProperty, value); }
+        }
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(PanKey), new PropertyMetadata(10));
+
+
+        // Using a DependencyProperty as the backing store for Y.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty YProperty =
+            DependencyProperty.Register("Y", typeof(int), typeof(PanKey), new PropertyMetadata(10));
+
+
+        // Using a DependencyProperty as the backing store for X.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty XProperty =
+            DependencyProperty.Register("X", typeof(int), typeof(PanKey), new PropertyMetadata(100));
+
+        public bool CheckLocation()
+        {
+            return X + 4 * Y + 1 == Value;
+        }
+    }
+}
